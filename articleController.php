9ba4bced1a91052633226faee9e88ccef9fccf8e
alfandirio <73221556+alfandirio@users.noreply.github.com>
2020-11-06 22:21:43 +0700

    comit init

---
+++ articleController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class articleController extends Controller
+{
+   
+
+    public function article ($page){
+  
+   return view('article', ['page'=>$page]);
+   public function add() 
+   { 
+       return view('addarticle'); 
+   } 
+   public function create(Request $request) 
+   { 
+       Article::create([ 
+           'title' => $request->title, 
+           'content' => $request->content, 
+           'featured_image' => $request->image 
+       ]); 
+       return redirect('/manage');     } 
+       protected $fillable = ['title','content','featured_image']; 
+       public function edit($id) 
+    { 
+        $article = Article::find($id); 
+        return view('editarticle',['article'=>$article]);     } 
+        <form action="/article/update/{{$article->id}}" method="post"> 
+        {{csrf_field()}} 
+        <input type="hidden" name="id" value="{{$article-
+>id}}"></br> 
+        <div class="form-group"> 
+            <label for="title">Judul</label>           
+                  <input type="text" class="form-control" 
+required="required" name="title" value="{{$article->title}}"></br> 
+        </div> 
+        <div class="form-group"> 
+            <label for="content">Content</label>               
+              <input type="text" class="form-control" required="required" name="content" value="{{$article-
+>content}}">
+</br> 
+        </div> 
+        <div class="form-group"> 
+            <label for="image">Feature Image</label>          
+                   <input type="text" class="form-control" required="required" name="image" value="{{$article-
+>featured_image}}"></br> 
+        </div> 
+        <button type="submit" name="edit" class="btn btnprimary float-right">Ubah Data</button> 
+        public function update($id, Request $request) 
+        { 
+            $article = Article::find($id); 
+            $article->title = $request->title; 
+            $article->content = $request->content; 
+            $article->featured_image = $request->image; 
+            $article->save(); S
+            return redirect('/manage');     } 
+            <a href="article/delete/{{ $a->id }}" class="badge badgedanger">Hapus</a> 
+            public function delete($id) 
+    { 
+        $article = Article::find($id); 
+        $article->delete();       
+          return redirect('/manage'); 
+    } 
+    
+    public function __construct() 
+    { 
+        $this->middleware(function($request, $next){ 
+            if(Gate::allows('manage-articles')) return $next($request);      
+               abort(403, 'Anda tidak memiliki cukup hak akses'); 
+        }); 
+    } 
+    <li class="nav-item {{ Route::is('manage') ? 'active' : '' }}"> 
+   @can('manage-articles') 
+   <a class="nav-link" href="{{ route('manage') }}">Kelola</a> 
+   @endcan 
+</li> 
+
+    
+   
+
+ 
+  
+}
+}
+
+
